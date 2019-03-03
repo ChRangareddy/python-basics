17887288d617bc238540010123f745801f29c5f9
chRangareddy <ch.rangareddy143@gmail.com>
2019-03-03 11:00:03 +0530

    python basics

---
+++ python basics.py
@@ -0,0 +1,43 @@
+Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 22:20:52) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("helloworld")
+helloworld
+>>> [1,2,3]+[4,5,6]
+[1, 2, 3, 4, 5, 6]
+>>> 4+5+6
+15
+>>> 4-2*5
+-6
+>>> 10/2
+5.0
+>>> 14//2
+7
+>>> 15%3
+0
+>>> 4*2+4
+12
+>>> [1,2,3,4,5]
+[1, 2, 3, 4, 5]
+>>> a=[1,4,5,6]
+>>> a[1:3]
+[4, 5]
+>>> a[0:3]
+[1, 4, 5]
+>>> a[0:4]
+[1, 4, 5, 6]
+>>> ('plant')
+'plant'
+>>> a=('words')
+>>> len(a)
+5
+>>> m=[1,2,3,4]
+>>> m.appendo(20)
+Traceback (most recent call last):
+  File "<pyshell#17>", line 1, in <module>
+    m.appendo(20)
+AttributeError: 'list' object has no attribute 'appendo'
+>>> 
+>>> m.append(20)
+>>> m
+[1, 2, 3, 4, 20]
+>>> 
